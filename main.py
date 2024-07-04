5eda39d5e13107dafc1a3ce7b1976124664dc6b0
Mainlst <mainlst@outlook.jp>
2024-07-04 19:11:22 +0900

    ResNet とりあえず動く

--- main.py
+++ main.py
@@ -18,22 +18,30 @@ from src.utils import set_seed
 def run(args: DictConfig):
     set_seed(args.seed)
     logdir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-    
+
     if args.use_wandb:
         wandb.init(mode="online", dir=logdir, project="MEG-classification")
 
     # ------------------
     #    Dataloader
     # ------------------
-    loader_args = {"batch_size": args.batch_size, "num_workers": args.num_workers}
-    
+    loader_args = {
+        "batch_size": args.batch_size,
+        "num_workers": args.num_workers,
+        "pin_memory": True,
+    }
+
     train_set = ThingsMEGDataset("train", args.data_dir)
     train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, **loader_args)
     val_set = ThingsMEGDataset("val", args.data_dir)
     val_loader = torch.utils.data.DataLoader(val_set, shuffle=False, **loader_args)
     test_set = ThingsMEGDataset("test", args.data_dir)
     test_loader = torch.utils.data.DataLoader(
-        test_set, shuffle=False, batch_size=args.batch_size, num_workers=args.num_workers
+        test_set,
+        shuffle=False,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=True,
     )
 
     # ------------------
@@ -50,64 +58,84 @@ def run(args: DictConfig):
 
     # ------------------
     #   Start training
-    # ------------------  
+    # ------------------
     max_val_acc = 0
+    patience = args.patience  # 早期終了のための忍耐力
+    epochs_no_improve = 1  # 改善が見られなかったエポック数
     accuracy = Accuracy(
         task="multiclass", num_classes=train_set.num_classes, top_k=10
     ).to(args.device)
-      
+
     for epoch in range(args.epochs):
         print(f"Epoch {epoch+1}/{args.epochs}")
-        
+
         train_loss, train_acc, val_loss, val_acc = [], [], [], []
-        
+
         model.train()
         for X, y, subject_idxs in tqdm(train_loader, desc="Train"):
             X, y = X.to(args.device), y.to(args.device)
 
             y_pred = model(X)
-            
+
             loss = F.cross_entropy(y_pred, y)
             train_loss.append(loss.item())
-            
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
+
             acc = accuracy(y_pred, y)
             train_acc.append(acc.item())
 
         model.eval()
         for X, y, subject_idxs in tqdm(val_loader, desc="Validation"):
             X, y = X.to(args.device), y.to(args.device)
-            
+
             with torch.no_grad():
                 y_pred = model(X)
-            
+
             val_loss.append(F.cross_entropy(y_pred, y).item())
             val_acc.append(accuracy(y_pred, y).item())
 
-        print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}")
+        print(
+            f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train acc: {np.mean(train_acc):.3f} | val loss: {np.mean(val_loss):.3f} | val acc: {np.mean(val_acc):.3f}"
+        )
         torch.save(model.state_dict(), os.path.join(logdir, "model_last.pt"))
         if args.use_wandb:
-            wandb.log({"train_loss": np.mean(train_loss), "train_acc": np.mean(train_acc), "val_loss": np.mean(val_loss), "val_acc": np.mean(val_acc)})
-        
+            wandb.log(
+                {
+                    "train_loss": np.mean(train_loss),
+                    "train_acc": np.mean(train_acc),
+                    "val_loss": np.mean(val_loss),
+                    "val_acc": np.mean(val_acc),
+                }
+            )
+
         if np.mean(val_acc) > max_val_acc:
             cprint("New best.", "cyan")
             torch.save(model.state_dict(), os.path.join(logdir, "model_best.pt"))
             max_val_acc = np.mean(val_acc)
-            
-    
+            epochs_no_improve = 0  # 改善があったのでリセット
+        else:
+            epochs_no_improve += 1  # 改善がなかったエポック数を増やす
+
+        # 早期終了のチェック
+        if epochs_no_improve >= patience:
+            print("Early stopping triggered")
+            break
+
     # ----------------------------------
     #  Start evaluation with best model
     # ----------------------------------
-    model.load_state_dict(torch.load(os.path.join(logdir, "model_best.pt"), map_location=args.device))
+    model.load_state_dict(
+        torch.load(os.path.join(logdir, "model_best.pt"), map_location=args.device)
+    )
 
-    preds = [] 
+    preds = []
     model.eval()
-    for X, subject_idxs in tqdm(test_loader, desc="Validation"):        
+    for X, subject_idxs in tqdm(test_loader, desc="Validation"):
         preds.append(model(X.to(args.device)).detach().cpu())
-        
+
     preds = torch.cat(preds, dim=0).numpy()
     np.save(os.path.join(logdir, "submission"), preds)
     cprint(f"Submission {preds.shape} saved at {logdir}", "cyan")
--- src/models.py
+++ src/models.py
@@ -6,11 +6,7 @@ from einops.layers.torch import Rearrange
 
 class BasicConvClassifier(nn.Module):
     def __init__(
-        self,
-        num_classes: int,
-        seq_len: int,
-        in_channels: int,
-        hid_dim: int = 128
+        self, num_classes: int, seq_len: int, in_channels: int, hid_dim: int = 128
     ) -> None:
         super().__init__()
 
@@ -26,34 +22,22 @@ class BasicConvClassifier(nn.Module):
         )
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        """_summary_
-        Args:
-            X ( b, c, t ): _description_
-        Returns:
-            X ( b, num_classes ): _description_
-        """
         X = self.blocks(X)
-
         return self.head(X)
 
 
 class ConvBlock(nn.Module):
     def __init__(
-        self,
-        in_dim,
-        out_dim,
-        kernel_size: int = 3,
-        p_drop: float = 0.1,
+        self, in_dim, out_dim, kernel_size: int = 3, p_drop: float = 0.1
     ) -> None:
         super().__init__()
-        
+
         self.in_dim = in_dim
         self.out_dim = out_dim
 
         self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
         self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
-        # self.conv2 = nn.Conv1d(out_dim, out_dim, kernel_size) # , padding="same")
-        
+
         self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
         self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
 
@@ -66,11 +50,7 @@ class ConvBlock(nn.Module):
             X = self.conv0(X)
 
         X = F.gelu(self.batchnorm0(X))
-
         X = self.conv1(X) + X  # skip connection
         X = F.gelu(self.batchnorm1(X))
-
-        # X = self.conv2(X)
-        # X = F.glu(X, dim=-2)
 
         return self.dropout(X)
